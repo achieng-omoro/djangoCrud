5a67baae589b97b79c3f6d0156a935d2ca611599
achieng omoro <omoromichael@gmail.com>
2023-11-08 20:39:14 +0300

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import driver, slider
+from . models import driver, Slider
 # Register your models here.
 
 
 admin.site.register(driver)
-admin.site.register(slider)
+admin.site.register(Slider)
